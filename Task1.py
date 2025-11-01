75b3265a98d29f07a11ff0804099301bf6b02266
BhuvanSuthar <anonymoususer16215@gmail.com>
2025-11-01 23:15:01 +0530

    initial commit

---
+++ Task1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter First Number : "))
+b=int(input("Enter Secound Number : "))
+print("Addition : ",a+b)
+print("Substraction : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
